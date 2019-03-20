3520866ee5dae4938452b74fd48b4416390de8f1
Ross Basarevych <basarevych@gmail.com>
2019-03-20 17:31:49 +0200

    Docs update

--- api/src/Routes/Redirect.js
+++ api/src/Routes/Redirect.js
@@ -41,7 +41,7 @@ class RedirectRoute extends EventEmitter {
           return res.redirect("https://github.com/basarevych/next-dashboard");
         case "benchmarks":
           return res.redirect(
-            "https://gtmetrix.com/reports/dashboard.daemon-notes.com/jtZDI2Jh"
+            "https://gtmetrix.com/reports/dashboard.daemon-notes.com/h0cdrot4"
           );
         case "responsiveness":
           return res.redirect(
