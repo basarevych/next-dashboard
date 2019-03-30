a587d39661432109efcbc5bb3bd14c9410c1e194
Ross Basarevych <basarevych@gmail.com>
2019-03-30 13:55:55 +0200

    Typo

--- common/constants.js
+++ common/constants.js
@@ -25,63 +25,63 @@ module.exports = {
       icon: "dashboard",
       menu: "MENU_DASHBOARD",
       title: "TITLE_DASHBOARD",
-      isAllowed: roles => _.includes(roles, roles.AUTHENTICATED)
+      isAllowed: userRoles => _.includes(userRoles, roles.AUTHENTICATED)
     },
     "/forms": {
       page: "/forms",
       icon: "forms",
       menu: "MENU_FORMS",
       title: "TITLE_FORMS",
-      isAllowed: roles => _.includes(roles, roles.AUTHENTICATED)
+      isAllowed: userRoles => _.includes(userRoles, roles.AUTHENTICATED)
     },
     "/charts": {
       page: "/charts",
       icon: "charts",
       menu: "MENU_CHARTS",
       title: "TITLE_CHARTS",
-      isAllowed: roles => _.includes(roles, roles.AUTHENTICATED)
+      isAllowed: userRoles => _.includes(userRoles, roles.AUTHENTICATED)
     },
     "/tables": {
       page: "/tables",
       icon: "tables",
       menu: "MENU_TABLES",
       title: "TITLE_TABLES",
-      isAllowed: roles => _.includes(roles, roles.AUTHENTICATED)
+      isAllowed: userRoles => _.includes(userRoles, roles.AUTHENTICATED)
     },
     "/maps": {
       page: "/maps",
       icon: "maps",
       menu: "MENU_MAPS",
       title: "TITLE_MAPS",
-      isAllowed: roles => _.includes(roles, roles.AUTHENTICATED)
+      isAllowed: userRoles => _.includes(userRoles, roles.AUTHENTICATED)
     },
     "/notifications": {
       page: "/notifications",
       icon: "notifications",
       menu: "MENU_NOTIFICATIONS",
       title: "TITLE_NOTIFICATIONS",
-      isAllowed: roles => _.includes(roles, roles.AUTHENTICATED)
+      isAllowed: userRoles => _.includes(userRoles, roles.AUTHENTICATED)
     },
     "/typography": {
       page: "/typography",
       icon: "typography",
       menu: "MENU_TYPOGRAPHY",
       title: "TITLE_TYPOGRAPHY",
-      isAllowed: roles => _.includes(roles, roles.AUTHENTICATED)
+      isAllowed: userRoles => _.includes(userRoles, roles.AUTHENTICATED)
     },
     "/icons": {
       page: "/icons",
       icon: "icons",
       menu: "MENU_ICONS",
       title: "TITLE_ICONS",
-      isAllowed: roles => _.includes(roles, roles.AUTHENTICATED)
+      isAllowed: userRoles => _.includes(userRoles, roles.AUTHENTICATED)
     },
     "/auth/profile": {
       page: "/auth/profile",
       title: "TITLE_PROFILE",
-      isAllowed: roles =>
-        _.includes(roles, roles.AUTHENTICATED) &&
-        !_.includes(roles, roles.ANONYMOUS)
+      isAllowed: userRoles =>
+        _.includes(userRoles, roles.AUTHENTICATED) &&
+        !_.includes(userRoles, roles.ANONYMOUS)
     },
     "/auth/verify": {
       page: "/auth/verify",
@@ -96,7 +96,7 @@ module.exports = {
       icon: "users",
       menu: "MENU_USERS",
       title: "TITLE_USERS",
-      isAllowed: roles => _.includes(roles, roles.ADMIN)
+      isAllowed: userRoles => _.includes(userRoles, roles.ADMIN)
     }
   },
   depts: {
